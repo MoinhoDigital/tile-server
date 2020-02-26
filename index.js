b571f00b1c04c02326c396aa3c694411bc341e74
luandro <luandro@gmail.com>
2020-02-26 19:32:53 -0300

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 3000
+app.use(cors())
 
 app.use(express.static('assets'))
 app.listen(port, () => console.log(`Map tiles being server on ${port}!`))
